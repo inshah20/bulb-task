2d7d7904eeaedc16294ce00d44481c7fce89df3d
inshah20 <inshahjamal26@gmail.com>
2025-04-22 00:16:54 +0500

    made changes in app.js:

---
+++ app.js
@@ -0,0 +1,25 @@
+let bulb = document.getElementById("b-off");
+let buttonOn = document.getElementById("click");
+let buttonOff = document.getElementById("no-click");
+
+
+
+buttonOn.addEventListener("click", function(){
+    bulb.src="./images/bulb-on.jpg"
+})
+
+
+
+buttonOff.addEventListener("click", function(){
+    bulb.src="./images/bulb-off.png"
+})
+
+// x ------------------------------- x ---------------------------- x ----------------------------------------x
+
+
+
+
+
+
+
+
